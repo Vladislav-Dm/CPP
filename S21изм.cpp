80d054466afeb3ccf3612c83abad23fbbcb459da
Vladislav-Dm <vladislav.vladimirovich2003@gmail.com>
2025-05-29 12:34:24 +0800

    Add files via upload

---
+++ S21изм.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <clocale>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int a, b;
+
+    std::cout << "Введи A: ";
+    std::cin >> a;
+    std::cout << "Введи B (больше A): ";
+    std::cin >> b;
+    std::ofstream f("output");
+
+    // Идем по числам от A до B
+    for (int i = a; i <= b; i++) {
+        // Записываем в файл число, умноженное на 3, и переходим на новую строку
+        f << i * 3 << std::endl;
+    }
+
+    return 0;
+}
